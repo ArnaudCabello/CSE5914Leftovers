a4a9feccff8a88332e405c3efb767c2e7d391fec
Gavin Adams <adams.2475@osu.edu>
2024-02-13 15:04:58 -0500

    Allow users to choose whether to see more details of a recipe

--- es.py
+++ es.py
@@ -5,15 +5,9 @@ import numpy as np
 
 es = Elasticsearch('https://localhost:9200', ca_certs="http_ca.crt", basic_auth=("elastic", "FWDZ0*ObFet0G4RlxY+4"))
 
-
-
 def isValidString(text):
     if text != None: return True
     return False
-
-def isValidInt(text):
-    if text > 3 or text < 1: return False
-    return True
 
 def handleInput():
     while True:
@@ -27,20 +21,38 @@ def handleQuery(columnName):
     query = {"match": {columnName: {"query": desiredKeyword}}}
     return query
 
+def showRecipes(docsList):
+    while True:
+        print("-----Recipe List-----")
+        for index, doc in enumerate (docsList):
+            recipe = doc["_source"]
+            name = recipe["Name"]
+            print("{}. {}".format(index + 1, name))
+        print("If you would like to see more details of a recipe listed, enter the number that was listed next to it. If you want to exit, enter 0.")
+        i = int(input())
+        if i == 0: break
+        if i < 1 or i > len(docsList): print("Invalid input. Please try again.")
+        else:
+            print(docsList[i-1]["_source"])
+
+
 def sendSearch(query):
     res = es.search (index="recipe", body={"query":query})
     print("Num hits: {}".format(len(res["hits"]["hits"])))
-    for doc in res["hits"]["hits"]:
-        print(doc)
+    showRecipes(res["hits"]["hits"])
+    
 
 while(True):
     print("Welcome to our recipe searcher! Select the option you'd like to use for your search.\n"
         "1. Exit\n2. Keywords\n3. Recipe Name")
     i = int(input())
-
-    if i == 1: break
     query = None
-    if i == 2: query = handleQuery("Keywords")
-    if i == 3: query = handleQuery("Name")
-    sendSearch(query)
+    match i:
+        case 1: break
+        case 2: query = handleQuery("Keywords")
+        case 3: query = handleQuery("Name")
+        case _: 
+            print("Invalid number. Please try again.")
+            break
+    if query != None: sendSearch(query)
 print("Thank you!")
