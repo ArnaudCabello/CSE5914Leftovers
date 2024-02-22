697d07e42b9e9f346a700062dd72fbcaa3457829
NickelUnderscore <nicholasy123@gmail.com>
2024-02-22 15:26:43 -0500

    Working loader to convert from csv to json

--- loader.py
+++ loader.py
@@ -1,43 +1,42 @@
 from elasticsearch import Elasticsearch
 
 import csv
+import json
+import ast
 
-def cleanList(string):
-    newString = string.replace("{", "[")
-    newString = newString.replace("}", "]")
-    newString = newString.replace("['", "[\"")
-    strToReplace = ["','", "\",'", "',\"", "' ,'", "\" ,'", "' ,\"", "', '", "\", '", "', \"", "' , '", "\" , '", "' , \""]
-    newString = newString.replace("']", "\"]")
-    for str in strToReplace:
-        newString = newString.replace(str, "\",\"")
-    return newString
+# Input and output file to use
+input_file = "recipes_w_search_terms.csv"
+output_file = "recipes.json"
 
-with open("test.csv", "r") as i:
-    rows = rows = len(list(csv.reader(i)))
+with open(input_file, "r") as input:
+        rows = len(input.readlines())
 
 lineStr = "["
-with open("recipes.json", "w") as o:
-    with open("test.csv", "r") as i:
-        reader = csv.reader(i)
-        # data = list(reader)
-        # rows = len(data)
-
+with open(output_file, "w") as output:
+    with open(input_file, "r") as input:
+        reader = csv.reader(input)
+        output.write("[")
+        # Create json line by line and output to json file
         for i, line in enumerate(reader):
-            lineStr += "{\"_op_type\": \"index\", \"_index\": \"recipe\","
-            lineStr += "\"id\":" + line[0] + ","
-            lineStr += "\"name\":\"" + line[1] + "\","
-            lineStr += "\"ingredients\":" + cleanList(line[3]) + ","
-            lineStr += "\"ingredients_raw_str\":" + cleanList(line[4]) + ","
-            lineStr += "\"serving_size\":\"" + line[5] + "\","
-            lineStr += "\"servings\":\"" + line[6] + "\","
-            lineStr += "\"steps\":" + cleanList(line[7]) + ","
-            lineStr += "\"tags\":" + cleanList(line[8]) + ","
-            termsStr = line[9]
-            lineStr += "\"search_terms\":" + cleanList(termsStr)
+            line9 = line[9].replace("{", "[")
+            line9 = line9.replace("}", "]")
+            json_line = {
+                "_op_type": "index",
+                "_index": "recipe",
+                "id": line[0],
+                "name": line[1],
+                "description": line[2],
+                "ingredients": ast.literal_eval(line[3]),
+                "ingredients_raw_str": ast.literal_eval(line[4]),
+                "serving_size": line[5],
+                "servings": line[6],
+                "steps": ast.literal_eval(line[7]),
+                "tags": ast.literal_eval(line[8]),
+                "search_terms": ast.literal_eval(line9)
+            }
+            output.write(json.dumps(json_line))
             if(reader.line_num == rows):
-                lineStr += "}]"
+                output.write("]")
             else:
-                lineStr += "},\n"
-            o.write(lineStr)
-            lineStr = ""
-
+                output.write(",\n")
+            print("Converted recipe id " + line[0])
