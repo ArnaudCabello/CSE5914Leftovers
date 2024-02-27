c94d1575d7f7c994a876a7d83a59ea6c71a2ebad
Josiah Cheung <Josiahcheung7@gmail.com>
2024-02-27 15:22:16 -0500

    add basic get request to elasticsearch

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
 from app import app
+from elasticsearch import Elasticsearch
+import config
 
+es = Elasticsearch('https://localhost:9200', ca_certs="http_ca.crt", basic_auth=("elastic", config.elastic_password))
+cluster_info = es.info()
+print(f"Connected to ElasticSearch cluster `{cluster_info['cluster_name']}`")
+# {es.info().body['cluster_name']}cluster_info = es.info()
 if __name__ == "__main__":
     app.run(debug=True)
+
+    
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from app import app
-
-from flask import render_template 
+from flask import Flask, request, render_template
+from elasticsearch import Elasticsearch
 
 @app.route('/')
 def home():
@@ -8,4 +8,14 @@ def home():
 
 @app.route('/results')
 def results():
+    query = request.args["q"].lower()
+    tokens = query.split('_')
+    for token in tokens:
+        print(token)
     return render_template('results.html')
+
+# TODO: Delete this when you have better instructions
+# STEPS:
+# 1. start elasticsearch cluster
+# 2. run bonsai_load (remember to check the config for the correct elastic password, and copy the http_Ca cert)
+# 2. run app.py, start developnig on views.py
