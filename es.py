c0915d0d75160491042cb3ac78d85a9788372e54
Gavin Adams <adams.2475@osu.edu>
2024-03-07 09:50:10 -0500

    Add pagination

--- es.py
+++ es.py
@@ -61,12 +61,30 @@ def showRecipes(docsList):
         else:
             print(docsList[int(i)-1]["_source"])
 
+def getTopTen(docsList):
+    page = 0
+    while True:
+        print("-----Recipe List-----")
+        print("Page {}".format(page + 1))
+        for i in range(np.min([len(docsList) - page*10, 10])):
+            doc = docsList[page*10 + i]
+            recipe = doc["_source"]
+            name = recipe["name"]
+            print("{}. {}".format(page*10 + i + 1, name))
+        print("If you would like to see more details of a recipe listed, enter the number that was listed next to it. If you want to exit, enter 0. If you want to see the next page of results, press -1")
+        i = input()
+        if int(i) > len(docsList): print("Invalid input. Please try again.")
+        elif int(i) == 0: break
+        elif int(i) < 0: page += 1
+        else:
+            print(docsList[int(i)-1]["_source"])
 
 def sendSearch(query):
     res = es.search (index="recipe", body={"query":query})
     numHits = len(res["hits"]["hits"])
+    print(res)
     print("Num hits: {}".format(numHits))
-    if numHits > 0: showRecipes(res["hits"]["hits"])
+    if numHits > 0: getTopTen(res["hits"]["hits"])
     
 def main():
     while(True):
@@ -86,11 +104,11 @@ def main():
 
 
 #This is an OR search
-#print("---OR SEARCH---")
-#query = handleShouldQuery("ingredients")
-#sendSearch(query)
+print("---OR SEARCH---")
+query = handleShouldQuery("ingredients")
+sendSearch(query)
 
 # This is an AND search
-print("---AND SEARCH---")
-query = handleMustQuery("ingredients")
-sendSearch(query)
+#print("---AND SEARCH---")
+#query = handleMustQuery("ingredients")
+#sendSearch(query)
