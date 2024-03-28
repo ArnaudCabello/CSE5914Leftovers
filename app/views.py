b96b0365ee8640ecf68cb664c6c1fa86df7882b3
Gavin Adams <adams.2475@osu.edu>
2024-03-28 11:56:47 -0400

    Include percentage of ingredients that match General code improvements

--- app/views.py
+++ app/views.py
@@ -18,13 +18,18 @@ def results():
     page = int(request.args.get("page", 1))
     query = request.args["q"].lower()
     tokens = query.split('_')
-    shouldTerms = list()
-    for token in tokens: shouldTerms.append({"term": {"ingredients": token}})
+    shouldTerms = [{"term": {"ingredients": token}} for token in tokens]
     res = es.search (index="recipe", body={"query": {"bool": {"should": shouldTerms}}})
     data = res["hits"]["hits"]
     start_index = (page - 1) * 10
     end_index = start_index + 10
     paginated_data = data[start_index:end_index]
+
+    # include the percent of ingredients matching in the data
+    for i, result in enumerate(paginated_data):
+        count = np.sum([1 if tokens.count(ingredient) > 0 else 0 for ingredient in  result["_source"]["ingredients"]])
+        paginated_data[i] = (result, np.round(count * 100 / len(result["_source"]["ingredients"]), 2))
+    
     return render_template('results.html', data=paginated_data, ingredients=tokens, page=page)
 
 # TODO: Delete this when you have better instructions
